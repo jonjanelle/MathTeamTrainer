3ee83d9c7fcb8746a2909391c6b9959167ee96db
Jon Janelle <jjanelle@g.harvard.edu>
2017-04-20 13:45:36 -0400

    validation changes

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Math Team Trainer',
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://a4.jjanelle.me'),
 
     /*
     |--------------------------------------------------------------------------
